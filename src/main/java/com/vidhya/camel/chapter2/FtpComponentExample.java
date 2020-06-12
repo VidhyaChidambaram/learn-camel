b6050c1e8d3ebe9f0a501d691a7cfb971634f057
Vidhya Chidambaram <vchidamb@redhat.com>
2020-06-12 17:03:00 -0400

    chapter#2 add call for message consumption and message body conversion

--- src/main/java/com/vidhya/camel/chapter2/FtpComponentExample.java
+++ src/main/java/com/vidhya/camel/chapter2/FtpComponentExample.java
@@ -22,27 +22,33 @@ public class FtpComponentExample {
                 new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
         CamelContext context = new DefaultCamelContext();
         context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-        context.addRoutes(new FtpRoute());
+        context.addRoutes(new FtpToJmsRoute());
         context.start();
         //delivers the same file multiple times as long as context is alive
-        Thread.sleep(10);
+        Thread.sleep(1000);
         context.stop();
     }
 }
 
-class FtpRoute extends RouteBuilder {
+class FtpToJmsRoute extends RouteBuilder {
 
-    private static final Log LOGGER = LogFactory.getLog(FtpRoute.class);
+    private static final Log LOGGER = LogFactory.getLog(FtpToJmsRoute.class);
 
     private static void process(Exchange exchange) {
-        LOGGER.info("We just downloaded" + exchange.getIn().getHeader("CamelFileName"));
+        LOGGER.info("We just downloaded : " + exchange.getIn().getHeader("CamelFileName"));
+    }
+
+    private static void printMessageContents(Exchange exchange) {
+        LOGGER.info("Going to print : " + exchange.getIn().getBody());
     }
     public void configure() throws Exception {
         from("sftp:test@localhost:2222/orders?password=password")
                 .log("connected to ftp server")
-                .process(FtpRoute::process)
+                .process(FtpToJmsRoute::process)
                 .log("File content : ${body}")
-                .to("jms:queue:testqueue")
-                .log("file sent to JMS queue successfully");
+                .to("jms:queue:testqueue");
+
+        from("jms:queue:testqueue").convertBodyTo(String.class, "utf-8").log("Consuming messages").process(FtpToJmsRoute::printMessageContents).log("Finishing the process");
+
     }
 }
